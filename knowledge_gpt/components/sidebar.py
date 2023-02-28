3adb7c251103bb90af6149249e21a091c5d4389d
nagateeg <123014410+nagateeg@users.noreply.github.com>
2023-02-27 23:01:54 -0800

    Sidebar changes
    No Input

--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -13,27 +13,8 @@ def sidebar():
             "2. Upload a pdf, docx, or txt file📄\n"
             "3. Ask a question about the document💬\n"
         )
-        api_key_input = st.text_input(
-            "OpenAI API Key",
-            type="password",
-            placeholder="Paste your OpenAI API key here (sk-...)",
-            help="You can get your API key from https://platform.openai.com/account/api-keys.",  # noqa: E501
-            value=st.session_state.get("OPENAI_API_KEY", ""),
-        )
+        
+        if True:
+            set_openai_api_key("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")
 
-        if api_key_input:
-            set_openai_api_key(api_key_input)
-
-        st.markdown("---")
-        st.markdown("# About")
-        st.markdown(
-            "📖KnowledgeGPT allows you to ask questions about your "
-            "documents and get accurate answers with instant citations. "
-        )
-        st.markdown(
-            "This tool is a work in progress. "
-            "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
-            "with your feedback and suggestions💡"
-        )
-        st.markdown("---")
-        st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
+        
